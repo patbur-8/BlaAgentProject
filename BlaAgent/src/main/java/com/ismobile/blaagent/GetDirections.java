3112eaedb14ad1c5fe24e44954eb082446604ed6
Alexandra Tsampikakis <alexandra.tsampikakis@gmail.com>
2013-07-31 12:26:55 +0200

    fghjh

--- BlaAgent/src/main/java/com/ismobile/blaagent/GetDirections.java
+++ BlaAgent/src/main/java/com/ismobile/blaagent/GetDirections.java
@@ -1,17 +1,12 @@
 package com.ismobile.blaagent;
 
-import android.util.Log;
-
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.charset.Charset;
 
