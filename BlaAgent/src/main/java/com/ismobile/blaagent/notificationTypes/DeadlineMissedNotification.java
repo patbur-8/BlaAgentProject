76eec3a965f0e3e2db37a9dc4cbc3520517a98e0
Alexandra Tsampikakis <alexandra.tsampikakis@gmail.com>
2013-08-05 16:50:27 +0200

    frrfg

--- BlaAgent/src/main/java/com/ismobile/blaagent/notificationTypes/DeadlineMissedNotification.java
+++ BlaAgent/src/main/java/com/ismobile/blaagent/notificationTypes/DeadlineMissedNotification.java
@@ -212,14 +212,13 @@ public class DeadlineMissedNotification extends NotificationType {
                 notiActions[0] = new NotificationAction(R.drawable.ic_launcher, "", resultIntent);
                 notiActions[1] = new NotificationAction(R.drawable.google_maps_logo, "", mapsIntent);
             } else {
-                notiActions = new NotificationAction[2];
+                notiActions = new NotificationAction[1];
                 notiActions[0] = new NotificationAction(R.drawable.ic_launcher, "", resultIntent);
             }
 
         } else if (missStatus == MISSNEXTASS) {
             notiActions = new NotificationAction[1];
             notiActions[0] = new NotificationAction(R.drawable.ic_launcher, "", resultIntent);
-            notiActions[1] = new NotificationAction(R.drawable.google_maps_logo, "", mapsIntent);
         } else {
             notiActions = new NotificationAction[1];
             notiActions[0] = new NotificationAction(R.drawable.ic_launcher, "", resultIntent);
